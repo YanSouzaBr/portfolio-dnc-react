4438ec38943572f5b0b8ff02a9b35983ca54f87a
Yan Silva <yansouzatricolor@gmail.com>
2023-08-02 14:57:39 -0300

    versao-2

--- src/componentes/sections/Presentation.js
+++ src/componentes/sections/Presentation.js
@@ -1,17 +1,52 @@
 import styles from './Presentation.module.css'
 import ButtonA from '../elements/ButtonA'
+import { useEffect, useState } from 'react'
 
 function Presentation (){
+    const [text, setText] = useState('');
+    const toRotate = ['Yan Souza', 'Desenvolvedor Full Stack'];
+    const [loop, setLoop] = useState(0);
+    const [isDeleting, setIsDeleting] = useState(false);
+    const period = 100;
+    const [delta, setDelta] = useState(100)
+
+    useEffect(()=>{
+        let ticker = setInterval(()=>{
+            toType()
+        }, delta)
+        return()=> {clearInterval(ticker)}
+
+    }, [text])
+
+    const toType = () =>{
+        let i = loop % toRotate.length;
+        let fullText = toRotate[i]
+        let updatedText = isDeleting ? fullText.substring(0,text.length-1) : fullText.substring(0,text.length+1)
+    
+        setText(updatedText);
+
+        if(!isDeleting && updatedText === fullText){
+            setIsDeleting(true);
+            setDelta(period);
+        }else if (isDeleting && updatedText === ''){
+            setIsDeleting(false);
+            setDelta(period);
+            setLoop(loop+1);
+        }
+            
+        
+    }
+
     return (
         <div className={styles.presentation} id='Presentation'>
             <p><strong>Bem-vindo ao meu Portfólio</strong></p>
-            <h1>Olá, eu sou Yan Souza!</h1>
+            <h1>Olá, eu sou {text}</h1>
             <p>
                 Sou um apaixonado por tecnologia e soluções inovadoras. <br/>
-                Como Product Manager, eu tenho o compromisso de resolver problemas <br/>
-                complexos e trazer resultados excepcionais para os negócios. <br/>
-                Meus projetos já geraram milhões de reais em receita anual <br/>
-                estou sempre em busca de novos desafios para superar. <br/>
+                Tenho muito a aprender ainda, porém nunca irei desistir, <br/>
+                gosto de aprender coisas novas e sempre estarei aberto <br/>
+                a oportunidades e aprendizados novos e totalmente diferentes <br/>
+                e estou sempre em busca de novos desafios para superar. <br/>
             </p>
             <ButtonA link='https://github.com/YanSouzaBr' text='Conecte-se comigo!'/>
         </div>
